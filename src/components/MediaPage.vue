6335190ae15b1d3061eb4bd1e32879ecb5bc37ea
5ddeveloper247 <127947055+5ddeveloper247@users.noreply.github.com>
2024-05-30 16:55:55 +0500

    thursdayWork

--- src/components/MediaPage.vue
+++ src/components/MediaPage.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
+const mediaData = ref([]);
 const currentTopVideo = ref(1);
 const currentBottomVideo = ref(1);
 
@@ -11,144 +12,150 @@ function showTopVideo(index) {
 function showBottomVideo(index) {
   currentBottomVideo.value = index;
 }
+
+onMounted(() => {
+  // Make API call
+  const base_url = 'http://127.0.0.1:8000';
+  fetch(base_url + '/api/frontend/home/media/get', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data.media);
+      mediaData.value = data.media; // Correct assignment
+
+      // Handle success (e.g., show a success message)
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // Handle error (e.g., show an error message)
+    });
+});
 </script>
 
+
 <template>
-    <div>
-      <!-- Video Section Top -->
-      <div class="media mx-md-5 px-md-5 mt-3 pt-5">
-        <div class="d-flex flex-column align-items-center justify-content-center pt-5">
-          <iframe
-            v-show="currentTopVideo === 1"
-            class="Uvideo-1"
-            src="https://www.youtube.com/embed/tgbNymZ7vqY"
-          ></iframe>
-          <iframe
-            v-show="currentTopVideo === 2"
-            class="Uvideo-2"
-            src="https://www.youtube.com/embed/3WrZMzqpFTc?si=PIqTmb58BLs8ftEQ"
-          ></iframe>
-          <iframe
-            v-show="currentTopVideo === 3"
-            class="Uvideo-3"
-            src="https://www.youtube.com/embed/wexzvClUcUk?si=e0btNBYWjsyozHtb"
-          ></iframe>
-          <a
-            href="https://www.youtube.com/embed/tgbNymZ7vqY"
-            target="_blank"
-            class="btn subscribe-btn d-flex align-items-center my-2"
-          >
-            <i class="fa-brands fa-youtube pe-2"></i>Subscribe
-          </a>
-        </div>
-      </div>
-  
-      <!-- Media Cards Top -->
-      <div class="row justify-content-center mx-md-5 mx-2 px-md-5 mt-3 media-cards">
-        <div class="col-md-3">
-          <div class="card media-card-1" @click="showTopVideo(1)">
-            <img src="../assets/Images/media-gallery-1.jpeg" alt="..." />
-            <div class="card-body">
-              <p class="card-text text-center px-2">
-                Inspiring Start Up Dreams Silicon Village Pakistan - Capital Smart
-                City
-              </p>
-            </div>
-            <i class="fa-brands fa-youtube"></i>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card media-card-2" @click="showTopVideo(2)">
-            <img src="../assets/Images/media-gallery-2.jpeg" alt="..." />
-            <div class="card-body">
-              <p class="card-text text-center px-2">
-                Capital Smart City Islamabad | Future Benefits Unveiled Silicon
-                Village | Development Updates | RBS
-              </p>
-            </div>
-            <i class="fa-brands fa-youtube"></i>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card media-card-3" @click="showTopVideo(3)">
-            <img src="../assets/Images/media-gallery-3.jpeg" alt="..." />
-            <div class="card-body">
-              <p class="card-text text-center px-2">
-                Captivating Aerial Tour of Capital Smart City Islamabad: Experience
-                the Beauty from Above! | RBS
-              </p>
-            </div>
-            <i class="fa-brands fa-youtube"></i>
-          </div>
-        </div>
-      </div>
-  
-      <!-- Video Section Bottom -->
-      <div class="media mx-md-5 px-md-5 mt-3">
-        <div class="d-flex flex-column align-items-center justify-content-center">
-          <iframe
-            v-show="currentBottomVideo === 1"
-            class="Uvideo-4"
-            src="https://www.youtube.com/embed/tgbNymZ7vqY"
-          ></iframe>
-          <iframe
-            v-show="currentBottomVideo === 2"
-            class="Uvideo-5"
-            src="https://www.youtube.com/embed/3WrZMzqpFTc?si=PIqTmb58BLs8ftEQ"
-          ></iframe>
-          <iframe
-            v-show="currentBottomVideo === 3"
-            class="Uvideo-6"
-            src="https://www.youtube.com/embed/wexzvClUcUk?si=e0btNBYWjsyozHtb"
-          ></iframe>
-          <a
-            href="https://www.youtube.com/embed/tgbNymZ7vqY"
-            target="_blank"
-            class="btn subscribe-btn d-flex align-items-center my-2"
-          >
-            <i class="fa-brands fa-youtube pe-2"></i>Subscribe
-          </a>
-        </div>
-      </div>
-  
-      <!-- Media Cards Bottom -->
-      <div class="row justify-content-center mx-md-5 mx-2 px-md-5 mt-3 media-cards">
-        <div class="col-md-3">
-          <div class="card media-card-4" @click="showBottomVideo(1)">
-            <img src="../assets/Images/media-gallery-1.jpeg" alt="..." />
-            <div class="card-body">
-              <p class="card-text text-center px-2">
-                Inspiring Start Up Dreams Silicon Village Pakistan - Capital Smart
-                City
-              </p>
-            </div>
-            <i class="fa-brands fa-youtube"></i>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card media-card-5" @click="showBottomVideo(2)">
-            <img src="../assets/Images/media-gallery-2.jpeg" alt="..." />
-            <div class="card-body">
-              <p class="card-text text-center px-2">
-                Capital Smart City Islamabad | Future Benefits Unveiled Silicon
-                Village | Development Updates | RBS
-              </p>
-            </div>
-            <i class="fa-brands fa-youtube"></i>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card media-card-6" @click="showBottomVideo(3)">
-            <img src="../assets/Images/media-gallery-3.jpeg" alt="..." />
-            <div class="card-body">
-              <p class="card-text text-center px-2">
-                Captivating Aerial Tour of Capital Smart City Islamabad: Experience
-                the Beauty from Above! | RBS
-              </p>
-            </div>
-            <i class="fa-brands fa-youtube"></i>
-          </div>
-        </div>
+  <div>
+
+
+    <!-- <li v-for="media in mediaData">
+      {{ media.url }}
+    </li> -->
+
+
+
+    <div class="media mx-md-5 px-md-5 mt-3 pt-5">
+      <div class="d-flex flex-column align-items-center justify-content-center pt-5">
+        <iframe v-show="currentTopVideo === 1" class="Uvideo-1"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+        <iframe v-show="currentTopVideo === 2" class="Uvideo-2"
+          src="https://www.youtube.com/embed/3WrZMzqpFTc?si=PIqTmb58BLs8ftEQ"></iframe>
+        <iframe v-show="currentTopVideo === 3" class="Uvideo-3"
+          src="https://www.youtube.com/embed/wexzvClUcUk?si=e0btNBYWjsyozHtb"></iframe>
+        <a href="https://www.youtube.com/embed/tgbNymZ7vqY" target="_blank"
+          class="btn subscribe-btn d-flex align-items-center my-2">
+          <i class="fa-brands fa-youtube pe-2"></i>Subscribe
+        </a>
       </div>
     </div>
-  </template>
+
+    <!-- Media Cards Top -->
+    <div class="row justify-content-center mx-md-5 mx-2 px-md-5 mt-3 media-cards">
+      <div class="col-md-3" v-for="media in mediaData">
+        <div class="card media-card-1" @click="showTopVideo(media.id)">
+          <img src="../assets/Images/media-gallery-1.jpeg" alt="..." />
+          <div class="card-body">
+            <p class="card-text text-center px-2">
+              {{ media.description }}
+            </p>
+          </div>
+          <i class="fa-brands fa-youtube"></i>
+        </div>
+      </div>
+
+      <!-- <div class="col-md-3">
+        <div class="card media-card-2" @click="showTopVideo(2)">
+          <img src="../assets/Images/media-gallery-2.jpeg" alt="..." />
+          <div class="card-body">
+            <p class="card-text text-center px-2">
+              Capital Smart City Islamabad | Future Benefits Unveiled Silicon
+              Village | Development Updates | RBS
+            </p>
+          </div>
+          <i class="fa-brands fa-youtube"></i>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card media-card-3" @click="showTopVideo(3)">
+          <img src="../assets/Images/media-gallery-3.jpeg" alt="..." />
+          <div class="card-body">
+            <p class="card-text text-center px-2">
+              Captivating Aerial Tour of Capital Smart City Islamabad: Experience
+              the Beauty from Above! | RBS
+            </p>
+          </div>
+          <i class="fa-brands fa-youtube"></i>
+        </div>
+      </div> -->
+    </div>
+
+    <!-- Video Section Bottom -->
+    <div class="media mx-md-5 px-md-5 mt-3">
+      <div class="d-flex flex-column align-items-center justify-content-center">
+        <iframe v-show="currentBottomVideo === 1" class="Uvideo-4"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+        <iframe v-show="currentBottomVideo === 2" class="Uvideo-5"
+          src="https://www.youtube.com/embed/3WrZMzqpFTc?si=PIqTmb58BLs8ftEQ"></iframe>
+        <iframe v-show="currentBottomVideo === 3" class="Uvideo-6"
+          src="https://www.youtube.com/embed/wexzvClUcUk?si=e0btNBYWjsyozHtb"></iframe>
+        <a href="https://www.youtube.com/embed/tgbNymZ7vqY" target="_blank"
+          class="btn subscribe-btn d-flex align-items-center my-2">
+          <i class="fa-brands fa-youtube pe-2"></i>Subscribe
+        </a>
+      </div>
+    </div>
+
+    <!-- Media Cards Bottom -->
+    <div class="row justify-content-center mx-md-5 mx-2 px-md-5 mt-3 media-cards">
+      <div class="col-md-3" v-for="media in mediaData">
+        <div class="card media-card-4" @click="showBottomVideo(media.id)">
+          <img src="../assets/Images/media-gallery-1.jpeg" alt="..." />
+          <div class="card-body">
+            <p class="card-text text-center px-2">
+              {{ media.description }}
+            </p>
+          </div>
+          <i class="fa-brands fa-youtube"></i>
+        </div>
+      </div>
+
+      <!-- <div class="col-md-3">
+        <div class="card media-card-5" @click="showBottomVideo(2)">
+          <img src="../assets/Images/media-gallery-2.jpeg" alt="..." />
+          <div class="card-body">
+            <p class="card-text text-center px-2">
+              Capital Smart City Islamabad | Future Benefits Unveiled Silicon
+              Village | Development Updates | RBS
+            </p>
+          </div>
+          <i class="fa-brands fa-youtube"></i>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card media-card-6" @click="showBottomVideo(3)">
+          <img src="../assets/Images/media-gallery-3.jpeg" alt="..." />
+          <div class="card-body">
+            <p class="card-text text-center px-2">
+              Captivating Aerial Tour of Capital Smart City Islamabad: Experience
+              the Beauty from Above! | RBS
+            </p>
+          </div>
+          <i class="fa-brands fa-youtube"></i>
+        </div>
+      </div> -->
+    </div>
+  </div>
+</template>
--- src/components/RequestForm.vue
+++ src/components/RequestForm.vue
@@ -1,88 +1,5 @@
-<script>
-export default{
-mounted(){
-    $(document).ready(function () {
-
-var current_fs, next_fs, previous_fs; //fieldsets
-var opacity;
-var current = 1;
-var steps = $("fieldset").length;
-
-setProgressBar(current);
-
-$(".next").click(function () {
-
-  current_fs = $(this).parent();
-  next_fs = $(this).parent().next();
-
-  //Add Class Active
-  $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-  //show the next fieldset
-  next_fs.show();
-  //hide the current fieldset with style
-  current_fs.animate({ opacity: 0 }, {
-    step: function (now) {
-      // for making fielset appear animation
-      opacity = 1 - now;
-
-      current_fs.css({
-        'display': 'none',
-        'position': 'relative'
-      });
-      next_fs.css({ 'opacity': opacity });
-    },
-    duration: 500
-  });
-  setProgressBar(++current);
-});
-
-$(".previous").click(function () {
-
-  current_fs = $(this).parent();
-  previous_fs = $(this).parent().prev();
-
-  //Remove class active
-  $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-  //show the previous fieldset
-  previous_fs.show();
-
-  //hide the current fieldset with style
-  current_fs.animate({ opacity: 0 }, {
-    step: function (now) {
-      // for making fielset appear animation
-      opacity = 1 - now;
-
-      current_fs.css({
-        'display': 'none',
-        'position': 'relative'
-      });
-      previous_fs.css({ 'opacity': opacity });
-    },
-    duration: 500
-  });
-  setProgressBar(--current);
-});
-
-function setProgressBar(curStep) {
-  var percent = parseFloat(100 / steps) * curStep;
-  percent = percent.toFixed();
-  $(".progress-bar")
-    .css("width", percent + "%")
-}
-
-$(".submit").click(function () {
-  return false;
-})
-
-});
-}
-}
-</script>
-
 <template>
-   <!-- ======================Multi Step Form================== -->
+  <!-- ======================Multi Step Form================== -->
   <div class="container-fluid call-to-action pt-5">
     <div class="row justify-content-center pt-md-5 pt-3">
       <div class="col-md-8 col-12 text-center p-0">
@@ -103,19 +20,23 @@ $(".submit").click(function () {
             <fieldset>
               <div class="form-card">
                 <div class="form-floating mb-3">
-                  <input type="text" class="form-control" id="floatingInput" placeholder="First Name">
+                  <input type="text" class="form-control" id="floatingInput" v-model="propertyRequestform.firstName"
+                    placeholder="First Name">
                   <label for="floatingInput">First Name</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="text" class="form-control" id="floatingInput" placeholder="Last Name">
+                  <input type="text" class="form-control" id="floatingInput" v-model="propertyRequestform.lastName"
+                    placeholder="Last Name">
                   <label for="floatingInput">Last Name</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="email" class="form-control" id="floatingInput" placeholder="sk@gmail.com">
+                  <input type="email" class="form-control" id="floatingInput" v-model="propertyRequestform.email"
+                    placeholder="sk@gmail.com">
                   <label for="floatingInput">Email address</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="number" class="form-control" id="floatingInput" placeholder="Phone">
+                  <input type="number" class="form-control" id="floatingInput" v-model="propertyRequestform.phone"
+                    placeholder="Phone">
                   <label for="floatingInput">Phone</label>
                 </div>
               </div>
@@ -128,37 +49,23 @@ $(".submit").click(function () {
                     <h5>Purpose:</h5>
                   </div>
                   <div class="col-md-2 mx-1 mt-3 ms-5">
-                    <button type="submit" class="btn main-button ms-md-3 px-md-2 py-0 mb-4 btn-round w-100">Sale</button>
+                    <button type="button" class="btn main-button ms-md-3 px-md-2 py-0 mb-4 btn-round w-100"
+                      @click="handlePurpose('Sale')">Sale</button>
                   </div>
                   <div class="col-md-2 mx-1 mt-3 ms-5">
-                    <button type="submit" class="btn main-button-2 px-md-2 py-0 mb-4 btn-round w-100">Rent</button>
+                    <button type="button" class="btn main-button-2 px-md-2 py-0 mb-4 btn-round w-100"
+                      @click="handlePurpose('Rent')">Rent</button>
                   </div>
                 </div>
                 <div class="row justify-content-start my-2">
                   <div class="col-md-10">
-                    <div class="d-flex flex-wrap justify-content-md-between justify-content-center align-items-center">
-                      <h5>Property Type:</h5>
-                      <div class="d-flex align-items-center my-2 ms-md-5 ms-3 me-md-0 me-1">
-                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                        <i class="fa-solid fa-house ps-3 pe-2"></i>
-                        <p><small>House</small></p>
-                      </div>
-                      <div class="d-flex align-items-center my-2 mx-md-0 mx-1">
-                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
-                        <i class="fa-solid fa-house-laptop ps-3 pe-2"></i>
-                        <p><small>Rent</small></p>
-                      </div>
-                      <div class="d-flex align-items-center my-2 mx-md-0 mx-1">
-                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3">
-                        <i class="fa-solid fa-building ps-3 pe-2"></i>
-                        <p><small>Appartment</small></p>
-                      </div>
-                      <div class="d-flex align-items-center my-2 mx-md-0 mx-1">
-                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4">
-                        <i class="fa-solid fa-shop ps-3 pe-2"></i>
-                        <p><small>Shop</small></p>
-                      </div>
-                    </div>
+                    <h5>Property Type:</h5>
+                    <select class="form-select" v-model="propertyRequestform.propertyType">
+                      <option value="" disabled selected>Select Property Type</option>
+                      <option value="House">House</option>
+                      <option value="Apartment">Apartment</option>
+                      <option value="Shop">Shop</option>
+                    </select>
                   </div>
                 </div>
               </div>
@@ -176,27 +83,32 @@ $(".submit").click(function () {
                   </div>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="text" class="form-control" id="floatingInput" placeholder="City">
+                  <input type="text" class="form-control" id="floatingInput" v-model="propertyRequestform.city"
+                    placeholder="City">
                   <label for="floatingInput">City</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="text" class="form-control" id="floatingInput" placeholder="Location">
+                  <input type="text" class="form-control" id="floatingInput" v-model="propertyRequestform.location"
+                    placeholder="Location">
                   <label for="floatingInput">Location</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="text" class="form-control" id="floatingInput" placeholder="Address">
+                  <input type="text" class="form-control" id="floatingInput" v-model="propertyRequestform.address"
+                    placeholder="Address">
                   <label for="floatingInput">Address</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="number" class="form-control" id="floatingInput" placeholder="Size">
+                  <input type="number" class="form-control" id="floatingInput" v-model="propertyRequestform.size"
+                    placeholder="Size">
                   <label for="floatingInput">Size</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="number" class="form-control" id="floatingInput" placeholder="Total Price">
+                  <input type="number" class="form-control" id="floatingInput" v-model="propertyRequestform.totalPrice"
+                    placeholder="Total Price">
                   <label for="floatingInput">Total Price</label>
                 </div>
               </div>
-              <input type="button" name="next" class="next action-button" value="Submit" />
+              <input type="button" name="next" class="next action-button" @click="handleSubmission" value="Submit" />
               <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
             </fieldset>
             <fieldset>
@@ -223,3 +135,135 @@ $(".submit").click(function () {
     </div>
   </div>
 </template>
+
+
+
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+  setup() {
+    const propertyRequestform = ref({
+      firstName: '',
+      lastName: '',
+      phone: '',
+      email: '',
+      city: '',
+      location: '',
+      address: '',
+      size: '',
+      totalPrice: '',
+      purpose: '',
+      propertyType: ''
+    });
+
+    const handleSubmission = () => {
+      // Perform validation if necessary
+      console.log('loca:', propertyRequestform.value);
+      const base_url = 'http://127.0.0.1:8000'
+      // Make API call
+      fetch(base_url + '/api/frontend/home/register/property', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(propertyRequestform.value)
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Success:', data);
+          // Handle success (e.g., show a success message)
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          // Handle error (e.g., show an error message)
+        });
+    }
+
+    const handlePurpose = (purpose) => {
+      propertyRequestform.value.purpose = purpose;
+
+    }
+
+    onMounted(() => {
+      $(document).ready(function () {
+        var current_fs, next_fs, previous_fs; // fieldsets
+        var opacity;
+        var current = 1;
+        var steps = $("fieldset").length;
+
+        setProgressBar(current);
+
+        $(".next").click(function () {
+          current_fs = $(this).parent();
+          next_fs = $(this).parent().next();
+
+          // Add Class Active
+          $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+          // Show the next fieldset
+          next_fs.show();
+          // Hide the current fieldset with style
+          current_fs.animate({ opacity: 0 }, {
+            step: function (now) {
+              // for making fieldset appear animation
+              opacity = 1 - now;
+
+              current_fs.css({
+                'display': 'none',
+                'position': 'relative'
+              });
+              next_fs.css({ 'opacity': opacity });
+            },
+            duration: 500
+          });
+          setProgressBar(++current);
+        });
+
+        $(".previous").click(function () {
+          current_fs = $(this).parent();
+          previous_fs = $(this).parent().prev();
+
+          // Remove class active
+          $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+          // Show the previous fieldset
+          previous_fs.show();
+
+          // Hide the current fieldset with style
+          current_fs.animate({ opacity: 0 }, {
+            step: function (now) {
+              // for making fieldset appear animation
+              opacity = 1 - now;
+
+              current_fs.css({
+                'display': 'none',
+                'position': 'relative'
+              });
+              previous_fs.css({ 'opacity': opacity });
+            },
+            duration: 500
+          });
+          setProgressBar(--current);
+        });
+
+        function setProgressBar(curStep) {
+          var percent = parseFloat(100 / steps) * curStep;
+          percent = percent.toFixed();
+          $(".progress-bar").css("width", percent + "%");
+        }
+
+        $(".submit").click(function () {
+          return false;
+        });
+      });
+    });
+
+    return {
+      propertyRequestform,
+      handleSubmission,
+      handlePurpose
+    };
+  }
+};
+</script>
